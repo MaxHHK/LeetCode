33622ee67e3337ee1e129691b1eb8492efe3078b
MaxHong <1913147093@qq.com>
2018-10-10 21:53:39 +0800

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by Max on 2018/10/10.
+//  Copyright © 2018 Max. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
